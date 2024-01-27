0f4c56c2d8fec69be06a719f8114ddfde1a957d1
DR <DR@jps-macbook-pro.lan>
2024-01-27 19:54:50 +0800

    unit test for main component

--- src/app/modules/home/main/main.component.spec.ts
+++ src/app/modules/home/main/main.component.spec.ts
@@ -1,8 +1,10 @@
 import { ComponentFixture, TestBed, inject } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 import { MainComponent } from './main.component';
-import { DomSanitizer } from '@angular/platform-browser';
-import { MatIconRegistry } from '@angular/material/icon';
+import { MatIconTestingModule } from '@angular/material/icon/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatIcon } from '@angular/material/icon';
+import { By } from '@angular/platform-browser';
 
 describe('MainComponent', () => {
   let component: MainComponent;
@@ -10,7 +12,8 @@ describe('MainComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [MainComponent]
+      declarations: [MainComponent, MatIcon],
+      imports: [RouterTestingModule, MatIconTestingModule, HttpClientModule],
     });
     fixture = TestBed.createComponent(MainComponent);
     component = fixture.componentInstance;
@@ -19,5 +22,54 @@ describe('MainComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('header section', () => {
+    it('should show heading 1', () => {
+      let element = fixture.debugElement.query(By.css('#heading-1'));
+      expect(element).toBeTruthy();
+    });
+
+    it('should show subheading', () => {
+      let element = fixture.debugElement.query(By.css('#subheading-1'));
+      expect(element).toBeTruthy();
+    });
+  });
+
+  describe('bullet points', () => {
+    it('should have 3 bullets', () => {
+      let elements = fixture.debugElement.queryAll(By.css('.bullet'));
+      expect(elements.length).toEqual(3);
+    });
+    it('should show 3 check icon', () => {
+      let elements = fixture.debugElement.queryAll(By.css('.check'));
+      expect(elements.length).toEqual(3);
+    });
+  });
+
+  it('should show take exam button', () => {
+    let element = fixture.debugElement.query(By.css('#take-quiz-btn'));
+    expect(element).toBeTruthy();
+  });
+  it('should show image', () => {
+    let element = fixture.debugElement.query(By.css('#header-img'));
+    expect(element).toBeTruthy();
+  });
+
+  describe('course overview', () => {
+    it('should have 5 courses', () => {
+      let element = fixture.debugElement.queryAll(By.css('.course'));
+      expect(element.length).toEqual(5);
+    });
+    it('should have 5 course code', () => {
+      let element = fixture.debugElement.queryAll(By.css('.course-code'));
+      expect(element.length).toEqual(5);
+
+    })
+    it('should have 5 course desc', () => {
+      let element = fixture.debugElement.queryAll(By.css('.course-desc'));
+      expect(element.length).toEqual(5);
+
+    })
   });
 });
